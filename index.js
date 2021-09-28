6b0ec94ffa4ef54065be00cdc317152a69588fc8
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-09-28 18:59:23 -0400

    built fetch request to back end

---
+++ index.js
@@ -0,0 +1,9 @@
+const port = 'http://localhost:3000';
+const playlistApi = new PlaylistApi(port);
+const songApi = new SongApi(port)
+
+
+
+// const playlistForm = document.getElementById('playlist-form')
+// const songForm = document.getElementById('song-form')
+
---
+++ src/playlistApi.js
@@ -0,0 +1,3 @@
+class PlaylistApi {
+    
+}
