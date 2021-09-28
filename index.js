c1699dac5c82dce7cd6aa0c5ffb17220da3c7cc8
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-09-28 19:11:15 -0400

    added to index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const port = 'http://localhost:3000';
 const playlistApi = new PlaylistApi(port);
-const songApi = new SongApi(port)
+const songApi = new SongApi(port);
 
+const songForm = document.getElementById('song-form');
+
+const titleValue = document.getElementById('song-title');
+const artistValue = document.getElementById('song-artist');
 
 
 // const playlistForm = document.getElementById('playlist-form')
-// const songForm = document.getElementById('song-form')
 
