012861a5d2d5f5cdf898d2ab03b20c32c41ecb3a
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-09-29 19:39:19 -0400

    added song constructor

--- src/song.js
+++ src/song.js
@@ -1,3 +1,12 @@
 class Song {
     static all = []
+
+    constructor({title, artist, id, playlistId}) {
+        this.title = title;
+        this.artist = artist;
+        this.id = id;
+        this.playlist_id = playlistId;
+
+        Song.all.push(this)
+    }
 }
