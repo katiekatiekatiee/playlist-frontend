cbbdb97107f20b72b0e3d80777647be1e4b4b7da
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-09-30 18:57:39 -0400

    added render function to songs

--- src/song.js
+++ src/song.js
@@ -1,5 +1,6 @@
 class Song {
     static all = []
+    static container = document.getElementById(`songs-container`)
 
     constructor({title, artist, id, playlistId}) {
         this.title = title;
@@ -8,12 +9,22 @@ class Song {
         this.playlist_id = playlistId;
         this.element = document.createElement("div");
         this.element.id = `song-${this.id}`;
+        this.element.dataset['id'] = id;
 
         Song.all.push(this);
     }
 
-    
+
     render() {
-    
+        this.element.innerHTML = `
+        <div>
+       $<span class="title">${this.title}</span>
+        <span class="artist">${this.artist}</span>
+       </div>
+       
+       <button class="edit" data-id= "${this.id}" >Edit</button>
+       <button class="delete" data-id= "${this.id}" >Delete</button>
+       `
+       return this.element
     }
 }
