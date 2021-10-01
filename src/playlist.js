99113b105989728f2c282fe5b27fff502635799b
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-09-30 21:40:06 -0400

    added render function to playlist

--- src/playlist.js
+++ src/playlist.js
@@ -1,10 +1,17 @@
 class Playlist {
-    // static all = []
+    static all = []
+    static playlistContainer = document.getElementById('playlist-container')
 
-    constructor({title, description, id, songs}) {
+    constructor({title, description, id}) {
         this.title = title;
         this.description = description;
         this.id = id;
-        this.songs = songs;
+        // this.songs = songs;
+    }
+
+    render(){
+        this.element.innerText = this.title 
+        this.element.id = `playlist-${this.id}`
+        return this.element
     }
 }
