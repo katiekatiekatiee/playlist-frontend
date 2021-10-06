a3b65916203e3d79b5543beb1d4a3c92648ecb94
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-10-05 21:03:57 -0400

    added update method to song

--- src/songApi.js
+++ src/songApi.js
@@ -48,6 +48,27 @@ class SongApi {
    
     }
 
+    updateSong(song){
+        const {title, artist, id} = song
+        const songInfo = {
+            title, 
+            artist
+        }
+
+        const configObject = {
+            method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify(songInfo)
+        }
+        
+        fetch(`${this.baseUrl}/${id}`, configObject)
+        .then(song.render())
+        
+    }
+
     deleteSong(e) {
         const id = e.target.dataset.id
         e.target.parentElement.remove()
